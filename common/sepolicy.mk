c0b92a647ab1577ca1b238243334c4803bb42b2a
PainKiller3 <ninadpatil100@gmail.com>
2021-10-11 13:50:40 +0530

    sepolicy: Adapt path for Palladium-OS
    Change-Id: I734f370d1a0db02b0e674de9efb6e5ae443fb530

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -16,25 +16,25 @@ endif
 endif
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/public
+    device/palladium/sepolicy/common/public
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/private
+    device/palladium/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/dynamic \
-    device/lineage/sepolicy/common/system
+    device/palladium/sepolicy/common/dynamic \
+    device/palladium/sepolicy/common/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/dynamic \
-    device/lineage/sepolicy/common/vendor
+    device/palladium/sepolicy/common/dynamic \
+    device/palladium/sepolicy/common/vendor
 endif
 
 # Selectively include legacy rules defined by the products
--include device/lineage/sepolicy/legacy-common/sepolicy.mk
+-include device/palladium/sepolicy/legacy-common/sepolicy.mk
 
 # Include atv rules on atv product
 ifeq ($(PRODUCT_IS_ATV), true)
-include device/lineage/sepolicy/atv/sepolicy.mk
+include device/palladium/sepolicy/atv/sepolicy.mk
 endif
